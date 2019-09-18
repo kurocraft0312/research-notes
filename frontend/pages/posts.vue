f96708176d2277d8e479209cae64f071b8fd1e34
kurocraft0312 <kurocraft0312@gmail.com>
2019-09-18 20:12:23 +0900

    pugとscssに対応しました。

---
+++ frontend/pages/posts.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+  section.container
+    ul
+      li(v-for="post in posts" :key="post.id") {{post.id}}:{{post.title}}、{{post.content}}、
+        img(:src="'http://localhost:1337/${post.cover.url}'")
+</template>
