3e212c4dbc261bc7b5982042917707a2baf2161d
kurocraft0312 <kurocraft0312@gmail.com>
2019-09-18 21:30:40 +0900

    レイアウトテンプレートのコンテナ位置を中央揃えに調整。

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,3 +1,3 @@
 <template lang="pug">
-
+  div(class="container mx-auto px-4")
 </template>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,13 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-  theme: {},
+  theme: {
+    // https://tailwindcss.com/docs/containers
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+  },
   variants: {},
   plugins: []
 }
