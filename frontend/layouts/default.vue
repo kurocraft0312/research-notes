c471ddeaac73411da0bf94dd432ab22c88d2b5a5
kurocraft0312 <kurocraft0312@gmail.com>
2019-09-19 19:27:39 +0900

    メニューを実装

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -12,10 +12,8 @@
             title Menu
               path(d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z")
       div(class="w-full block flex-grow lg:flex lg:items-center lg:w-auto")
-        div(class="text-sm lg:flex-grow")
-          a(href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4") Docs
-          a(href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4") Examples
-          a(href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white") Blog
+        div(v-for="menu in menus" :key="menu.key" class="text-sm lg:flex-grow")
+          a(:href="menu.url" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4") {{menu.title}}
         //- div(v-for="menu in menus" :key="menu.key" class="text-sm lg:flex-grow")
         //-   a(:href="menu.url")
     //- contents
@@ -36,20 +34,20 @@
 export default {
   data() {
     return {
-      // menus: [
-      //   {
-      //     title:'HOME',
-      //     url: '/',
-      //   },
-      //   {
-      //     title:'ABOUT',
-      //     url: '/about',
-      //   },
-      //   {
-      //     title:'CONTACT',
-      //     url: '/contact',
-      //   },
-      // ],
+      menus: [
+        {
+          title:'HOME',
+          url: '/',
+        },
+        {
+          title:'ABOUT',
+          url: '/about',
+        },
+        {
+          title:'CONTACT',
+          url: '/contact',
+        },
+      ],
       articles: [
         {
           eyecatch: '/test.jpg',
