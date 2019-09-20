56bedacf99aa4279ab1bf8980f8a81ef2fd4e9da
kurocraft0312 <kurocraft0312@gmail.com>
2019-09-20 11:05:54 +0900

    いらないコメントアウト削除。

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -14,8 +14,6 @@
       div(class="w-full block flex-grow lg:flex lg:items-center lg:w-auto")
         div(v-for="menu in menus" :key="menu.key" class="text-sm")
           a(:href="menu.url" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4") {{menu.title}}
-        //- div(v-for="menu in menus" :key="menu.key" class="text-sm lg:flex-grow")
-        //-   a(:href="menu.url")
     //- contents
     div(class="container mx-auto px-4")
       div(class="flex content-center flex-wrap")
